3e63e69e36fe0ec11f3a39775cce5b6b9a456886
matiassalmoiraghi <32499594+matiassalmoiraghi@users.noreply.github.com>
2018-10-09 15:30:14 -0300

    Add files via upload

--- APP/GettyeConnectDLL/IntegracionGetty/Form1.vb
+++ APP/GettyeConnectDLL/IntegracionGetty/Form1.vb
@@ -42,9 +42,6 @@ Public Class Form1
 
     End Sub
 
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-    End Sub
 
     Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
         Dim objGeneral As New GettyeConnectDLL.IntegraGPXML
